ec54e963d10fcaa30c6a9d88434c5ce9df9a9bc0
rinel <rineliniguezsosa@gmail.com>
2023-06-12 16:10:51 -0600

    feat(MuiTypography): creando multiples componentes Typography
    estableciendo diferentes fuentes y tamaños mediante la prop variant

--- src/Components/MuiTypography/MuiTypography.tsx
+++ src/Components/MuiTypography/MuiTypography.tsx
@@ -2,6 +2,19 @@ import { Typography } from "@mui/material"
 
 export const MuiTypography = () => {
   return (
-    <div>MuiTypography</div>
+    <div>
+        <Typography variant="h1">Rinel variant h1</Typography>
+        <Typography variant="h2">Rinel variant h2</Typography>
+        <Typography variant="h3">Rinel variant h3</Typography>
+        <Typography variant="h4">Rinel variant h4</Typography>
+        <Typography variant="h5" component="h1">Rinel variant h5</Typography>
+        <Typography variant="h6">Rinel variant h6</Typography>
+
+        <Typography variant="subtitle1"> variant Subtitle 1</Typography>
+        <Typography variant="subtitle2"> variant Subtitle 2</Typography>
+
+        <Typography variant="body1"></Typography>
+        <Typography variant="body2"></Typography>
+    </div>
   )
 }
