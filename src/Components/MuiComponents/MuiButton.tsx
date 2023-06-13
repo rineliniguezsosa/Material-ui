f55bab2777842f91ae3a364c7150c7057a7c3cb5
rinel <rineliniguezsosa@gmail.com>
2023-06-12 18:28:06 -0600

    feat(MuiButton): agregando el componente Stack
    agregando la prop spacing con el valor a 4 y añiendo un tabulador al componente Stack hijo

--- src/Components/MuiComponents/MuiButton.tsx
+++ src/Components/MuiComponents/MuiButton.tsx
@@ -2,10 +2,12 @@ import { Stack,Button } from '@mui/material'
 
 export const MuiButton = () => {
   return (
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
     </Stack>
   )
 }
