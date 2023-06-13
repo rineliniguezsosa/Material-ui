3c5fe9dbbb230be5e04d3c6d5e896801face9ae9
rinel <rineliniguezsosa@gmail.com>
2023-06-12 18:01:11 -0600

    feat(App): importando el componente MuiButton y eliminando la importacion del componente MuiTypography
    cambiando el componente  MuiTypography por MuiButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { MuiTypography } from "./Components"
+import { MuiButton } from "./Components"
 
 function App() {
   return (
     <>
-      <MuiTypography/>
+      <MuiButton/>
     </>
   )
 }
