8bc31ce57fe537ebfdd12f96c0a06271bb3b17e4
rinel <rineliniguezsosa@gmail.com>
2023-06-12 18:38:07 -0600

    feat(MuiButton): agregando multiples Button components
    agregando las prop variant como contained y color con los valores primary,secondary,error,warning,info y success

--- src/Components/MuiComponents/MuiButton.tsx
+++ src/Components/MuiComponents/MuiButton.tsx
@@ -9,7 +9,12 @@ export const MuiButton = () => {
         <Button variant="outlined">Outlined</Button>
       </Stack>
       <Stack spacing={2} direction="row">
-
+        <Button variant="contained" color="primary">primary</Button>
+        <Button variant="contained" color="secondary">secondary</Button>
+        <Button variant="contained" color="error">error</Button>
+        <Button variant="contained" color="warning">warning</Button>
+        <Button variant="contained" color="info">info</Button>
+        <Button variant="contained" color="success">success</Button>
       </Stack>
     </Stack>
   )
