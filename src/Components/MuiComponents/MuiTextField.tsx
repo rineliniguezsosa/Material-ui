3be8b205121aca9d4efec62ed2655cd531c42b24
rinel <rineliniguezsosa@gmail.com>
2023-06-14 11:52:51 -0600

    feat(MuiTextField): agregando los componentes TextField con la prop variant standard,outlined y filled

--- src/Components/MuiComponents/MuiTextField.tsx
+++ src/Components/MuiComponents/MuiTextField.tsx
@@ -5,7 +5,9 @@ export const MuiTextField = () => {
   return (
     <Stack spacing={4}>
         <Stack direction="row" spacing={2}>
-            <TextField label="Name"/>
+            <TextField label="Name" variant="standard"/>
+            <TextField label="Name" variant="outlined"/>
+            <TextField label="Name" variant="filled"/>
         </Stack>
     </Stack>
   )
