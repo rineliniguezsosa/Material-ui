5cd79268b3757363ef8d2890ee95c7a4936e3736
rinel <rineliniguezsosa@gmail.com>
2023-06-14 13:52:53 -0600

    feat(MuiSelect): estableciendo las props value como el valor inicial ciudad y agregando el evento onChange donde ejecutaremos handlechange

--- src/Components/MuiComponents/MuiSelect.tsx
+++ src/Components/MuiComponents/MuiSelect.tsx
@@ -6,7 +6,7 @@ export const MuiSelect = () => {
     const [ciudad, setCiudad] = useState('')
   return (
     <Box width="200px">
-        <TextField label="Selecciona tu país" select>
+        <TextField label="Selecciona tu país" value={ciudad} onChange={handlechange} select>
             <MenuItem value="mexico">México</MenuItem>
             <MenuItem value="usa">USA</MenuItem>
             <MenuItem value="australia">Australia</MenuItem>
