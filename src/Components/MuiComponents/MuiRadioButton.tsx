241093542f53f718e39908cb8cabea9f4afd8934
rinel <rineliniguezsosa@gmail.com>
2023-06-14 17:31:02 -0600

    feat(MuiRadioButton): estableciendo el componente FormControlLabel
    estableciendo las props control como el componente Radio y agregando el titulo "3-5" mediante la prop label, prop value sera igual a "3-5"

--- src/Components/MuiComponents/MuiRadioButton.tsx
+++ src/Components/MuiComponents/MuiRadioButton.tsx
@@ -8,6 +8,7 @@ export const MuiRadioButton = () => {
           <FormLabel id="job-experience-group-label">Años de experiencia</FormLabel>
           <RadioGroup name="job-experience-group" aria-labelledby="job-experience-group-label">
             <FormControlLabel control={<Radio/>} label="0-2" value="0-2" />
+            <FormControlLabel control={<Radio/>} label="3-5" value="3-5"/>
           </RadioGroup>
       </FormControl>
     </Box>
