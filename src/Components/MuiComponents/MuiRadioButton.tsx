7078ce058571f218cde81b440a779740c07f08c2
rinel <rineliniguezsosa@gmail.com>
2023-06-14 17:03:02 -0600

    feat(MuiRadioButton): estableciendo el componente FormLabel con el titulo Años de experiencia
    prop id con el valor de job-experience-group-label

--- src/Components/MuiComponents/MuiRadioButton.tsx
+++ src/Components/MuiComponents/MuiRadioButton.tsx
@@ -5,7 +5,7 @@ export const MuiRadioButton = () => {
   return (
     <Box>
       <FormControl>
-        
+          <FormLabel id="job-experience-group-label">Años de experiencia</FormLabel>
       </FormControl>
     </Box>
   )
