bc5a9b7380e7438b82335e54b5eaec9db41d6868
rinel <rineliniguezsosa@gmail.com>
2023-06-14 13:38:45 -0600

    feat(MuiSelect): estableciendo el componente TextField
    props label="Selecciona tu país" como el titulo y la prop select para establecer el componente TextField de tipo select

--- src/Components/MuiComponents/MuiSelect.tsx
+++ src/Components/MuiComponents/MuiSelect.tsx
@@ -4,7 +4,9 @@ import { Box,MenuItem,TextField } from "@mui/material"
 export const MuiSelect = () => {
   return (
     <Box width="200px">
+        <TextField label="Selecciona tu país" select>
 
+        </TextField>
     </Box>
   )
 }
