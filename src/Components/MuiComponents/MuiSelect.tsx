d7fee65e18c09925e9a41b52c65713b385f81140
rinel <rineliniguezsosa@gmail.com>
2023-06-14 13:44:33 -0600

    feat(MuiSelect): estableciendo multiples componentes MenuItem con los titulos México,USA y Australia

--- src/Components/MuiComponents/MuiSelect.tsx
+++ src/Components/MuiComponents/MuiSelect.tsx
@@ -5,7 +5,9 @@ export const MuiSelect = () => {
   return (
     <Box width="200px">
         <TextField label="Selecciona tu país" select>
-
+            <MenuItem value="mexico">México</MenuItem>
+            <MenuItem value="usa">USA</MenuItem>
+            <MenuItem value="australia">Australia</MenuItem>
         </TextField>
     </Box>
   )
