81871ce6e3a08007a8afed3f17f949270bb35ec1
rinel <rineliniguezsosa@gmail.com>
2023-06-15 14:43:26 -0600

    feat(Muicheckbox): estableciendo lod componentes FormControlLabel
    propiedades label sera el titulo a mostrar y el control sera como el componente el cual sera como Checkbox

--- src/Components/MuiComponents/MuiCheckbox.tsx
+++ src/Components/MuiComponents/MuiCheckbox.tsx
@@ -4,7 +4,9 @@ export const MuiCheckbox = () => {
   return (
     <Box>
         <Box>
-            
+            <FormControlLabel control={<Checkbox/>} label="Acepta las condicones y terminos de privacidad">
+                
+            </FormControlLabel>
         </Box>
     </Box>
   )
