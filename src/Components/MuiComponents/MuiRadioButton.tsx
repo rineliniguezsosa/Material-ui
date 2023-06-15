cd948923e91b701cd02df2267cb60d6384e86a9e
rinel <rineliniguezsosa@gmail.com>
2023-06-15 14:10:25 -0600

    feat(MuiRadioButton): estableciendo las props value y el evento onChange
    la prop value se establecio como el valor inicial del state value y el evento onChange ejecuta por referencia la función handlechange

--- src/Components/MuiComponents/MuiRadioButton.tsx
+++ src/Components/MuiComponents/MuiRadioButton.tsx
@@ -8,7 +8,7 @@ export const MuiRadioButton = () => {
     <Box>
       <FormControl>
           <FormLabel id="job-experience-group-label">Años de experiencia</FormLabel>
-          <RadioGroup name="job-experience-group" aria-labelledby="job-experience-group-label">
+          <RadioGroup value={value} onChange={handlechange} name="job-experience-group" aria-labelledby="job-experience-group-label">
             <FormControlLabel control={<Radio/>} label="0-2" value="0-2" />
             <FormControlLabel control={<Radio/>} label="3-5" value="3-5"/>
           </RadioGroup>
