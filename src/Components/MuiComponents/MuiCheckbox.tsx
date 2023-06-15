8ec43a23476c6bfbe53ac465579810d5ced3a684
rinel <rineliniguezsosa@gmail.com>
2023-06-15 14:48:27 -0600

    fix(MuiCheckbox): corrigiendo el componente FormControlLabel

--- src/Components/MuiComponents/MuiCheckbox.tsx
+++ src/Components/MuiComponents/MuiCheckbox.tsx
@@ -6,9 +6,7 @@ export const MuiCheckbox = () => {
   return (
     <Box>
         <Box>
-            <FormControlLabel control={<Checkbox/>} label="Acepta las condicones y terminos de privacidad">
-
-            </FormControlLabel>
+            <FormControlLabel control={<Checkbox/>} label="Acepta las condicones y terminos de privacidad" />
         </Box>
     </Box>
   )
