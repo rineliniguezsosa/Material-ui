7938904fc227403253150a2194fff26ad43d73e6
rinel <rineliniguezsosa@gmail.com>
2023-06-15 14:20:11 -0600

    feat(MuiRadioButton): impriendo en consola el valor de value

--- src/Components/MuiComponents/MuiRadioButton.tsx
+++ src/Components/MuiComponents/MuiRadioButton.tsx
@@ -4,7 +4,7 @@ import { useState,ChangeEvent } from 'react';
 
 export const MuiRadioButton = () => {
   const [value, setValue] = useState('')
-
+  console.log(value)
   const handlechange = (event:ChangeEvent<HTMLInputElement>) =>{
     setValue(event.target.value)
   }
