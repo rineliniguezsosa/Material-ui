a3ae96336e386558125cd309e689b4d9b395f418
rinel <rineliniguezsosa@gmail.com>
2023-06-15 14:44:57 -0600

    feat(MuiCheckbox): importando el hook useState

--- src/Components/MuiComponents/MuiCheckbox.tsx
+++ src/Components/MuiComponents/MuiCheckbox.tsx
@@ -1,11 +1,12 @@
 import { Box,Checkbox,FormControlLabel } from '@mui/material'
+import { useState } from 'react';
 
 export const MuiCheckbox = () => {
   return (
     <Box>
         <Box>
             <FormControlLabel control={<Checkbox/>} label="Acepta las condicones y terminos de privacidad">
-                
+
             </FormControlLabel>
         </Box>
     </Box>
