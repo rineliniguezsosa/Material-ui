5c8e9c2c63352d2f669b12677b6e0de576366a66
rinel <rineliniguezsosa@gmail.com>
2023-06-16 16:58:38 -0600

    feat(MuiAutocomplete): estableciendo el componente Autocomplete
    props options el cual sera un array llamado skills,renderInput renderizara el componente TextField en donde propagaremos los params

--- src/Components/MuiComponents/MuiAutocomplete.tsx
+++ src/Components/MuiComponents/MuiAutocomplete.tsx
@@ -4,7 +4,7 @@ import { Stack,Autocomplete,TextField } from "@mui/material"
 export const MuiAutocomplete = () => {
   return (
     <Stack spacing={2} width="250px">
-
+        <Autocomplete options={skills} renderInput={(params)=><TextField {...params} label="Skills"/>}/>
     </Stack>
   )
 }
