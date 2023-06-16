271b43a9fd5624fcc0858d9889856b998a5c10f0
rinel <rineliniguezsosa@gmail.com>
2023-06-16 14:23:38 -0600

    feat(MuiSwitch): creando la arrow function handlechange

--- src/Components/MuiComponents/MuiSwitch.tsx
+++ src/Components/MuiComponents/MuiSwitch.tsx
@@ -4,6 +4,10 @@ import { useState,ChangeEvent } from 'react';
 
 export const MuiSwitch = () => {
     const [checked, setChecked] = useState(false)
+
+    const handlechange = ()=>{
+        
+    }
   return (
     <Box>
         <FormControlLabel label="Dark mode" control={<Switch checked={checked} onChange={handlechange}/>}/>
