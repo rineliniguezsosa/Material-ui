c8bd136cca507742ea2e14067775c34b17398386
rinel <rineliniguezsosa@gmail.com>
2023-06-16 14:13:34 -0600

    feat(MuiSwitch): estableciendo el componente FormControlLabel
    propiedades label con el titulo "Dark mode" control={<Switch/>} como la instancia a la cual seremos

--- src/Components/MuiComponents/MuiSwitch.tsx
+++ src/Components/MuiComponents/MuiSwitch.tsx
@@ -4,7 +4,7 @@ import { Box,FormControlLabel,Switch } from "@mui/material"
 export const MuiSwitch = () => {
   return (
     <Box>
-        
+        <FormControlLabel label="Dark mode" control={<Switch/>}/>
     </Box>
   )
 }
