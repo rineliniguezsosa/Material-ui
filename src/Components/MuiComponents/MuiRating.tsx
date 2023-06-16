23592252afca3f8ef40f42584ef9c00b82d3163b
rinel <rineliniguezsosa@gmail.com>
2023-06-16 14:57:14 -0600

    feat(MuiRating): estableciendo el hook useState
    inicializando el state value como null y especificando los valores futuros del state como number ó null

--- src/Components/MuiComponents/MuiRating.tsx
+++ src/Components/MuiComponents/MuiRating.tsx
@@ -2,6 +2,7 @@ import { Stack,Rating } from '@mui/material'
 import { useState,ChangeEvent } from 'react';
 
 export const MuiRating = () => {
+    const [value, setValue] = useState<number | null>(null)
   return (
     <Stack>
         
