5bd264c5c5d192e3016587d09bdc8bea4cfdd92a
rinel <rineliniguezsosa@gmail.com>
2023-06-16 15:53:35 -0600

    feat(MuiRating): imprimiendo con consola el valor del state value

--- src/Components/MuiComponents/MuiRating.tsx
+++ src/Components/MuiComponents/MuiRating.tsx
@@ -3,6 +3,8 @@ import { useState,ChangeEvent } from 'react';
 
 export const MuiRating = () => {
     const [value, setValue] = useState<number | null>(null)
+
+    console.log(value)
     
     const handlechange = (event:ChangeEvent<{}>,newvalue:number | null)=>{
         setValue(newvalue)
