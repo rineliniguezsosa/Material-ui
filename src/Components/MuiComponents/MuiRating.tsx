3f2a043c6ee7cd968c1ca07927bb81de069ec8c2
rinel <rineliniguezsosa@gmail.com>
2023-06-16 15:25:53 -0600

    feat(MuiRating): estableciendo las prop value y el evento onChange en el componente Rating
    la prop value tendra el valor del state value y el evento onChange tendra por referencia el nombre de la funcion hanldechange

--- src/Components/MuiComponents/MuiRating.tsx
+++ src/Components/MuiComponents/MuiRating.tsx
@@ -5,7 +5,7 @@ export const MuiRating = () => {
     const [value, setValue] = useState<number | null>(null)
   return (
     <Stack>
-        
+        <Rating value={value} onChange={handlechange}/>
     </Stack>
   )
 }
