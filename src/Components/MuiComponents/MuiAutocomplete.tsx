95cfda8b94d50c55a5a9f2776c837239942ddebd
rinel <rineliniguezsosa@gmail.com>
2023-06-16 17:03:03 -0600

    feat(MuiAutocomplete): creando el array skills
    tipando el array skils por un array de strings

--- src/Components/MuiComponents/MuiAutocomplete.tsx
+++ src/Components/MuiComponents/MuiAutocomplete.tsx
@@ -2,6 +2,7 @@ import { Stack,Autocomplete,TextField } from "@mui/material"
 
 
 export const MuiAutocomplete = () => {
+    const skills:string[] = ['HTML','Javascript','Typescript','React Formik','React Router']
   return (
     <Stack spacing={2} width="250px">
         <Autocomplete options={skills} renderInput={(params)=><TextField {...params} label="Skills"/>}/>
