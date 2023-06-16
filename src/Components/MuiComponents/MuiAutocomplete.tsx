95d95ccd9de4765a6d6a41e73c56c07d336f470f
rinel <rineliniguezsosa@gmail.com>
2023-06-16 17:34:42 -0600

    feat(MuiAutocomplete): estableciendo el hook useState
    inicializando el state value como null y tipando los valores futuros como string ó null

--- src/Components/MuiComponents/MuiAutocomplete.tsx
+++ src/Components/MuiComponents/MuiAutocomplete.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 
 export const MuiAutocomplete = () => {
     const skills:string[] = ['HTML','Javascript','Typescript','React Formik','React Router']
+
+    const [value, setValue] = useState<string | null>(null)
   return (
     <Stack spacing={2} width="250px">
         <Autocomplete options={skills} renderInput={(params)=><TextField {...params} label="Skills"/>}/>
