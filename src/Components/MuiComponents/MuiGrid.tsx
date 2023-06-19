9c1d63fb51567f9129846018fd5660ee84259297
rinel <rineliniguezsosa@gmail.com>
2023-06-19 17:26:06 -0600

    feat(MuiGrid): estableciendo multiples componentes Box con los titulos item 1, item 2 y item 3

--- src/Components/MuiComponents/MuiGrid.tsx
+++ src/Components/MuiComponents/MuiGrid.tsx
@@ -6,13 +6,13 @@ export const MuiGrid = () => {
     <div>
         <Grid container>
             <Grid item>
-                
+               <Box>item 1</Box> 
             </Grid>
             <Grid item>
-                
+               <Box>item 2</Box> 
             </Grid>
             <Grid item>
-                
+               <Box>item 3</Box> 
             </Grid>
         </Grid> 
     </div>
