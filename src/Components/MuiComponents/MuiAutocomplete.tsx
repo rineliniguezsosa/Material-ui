d85c113f668c3625214dd7b968aaeb75c9e1dbd0
rinel <rineliniguezsosa@gmail.com>
2023-06-19 13:10:54 -0600

    feat(MuiAutocomplete): creando la arrow function handlechange
    estableciendo los argumentos event y newvalue y tipando los argumentos

--- src/Components/MuiComponents/MuiAutocomplete.tsx
+++ src/Components/MuiComponents/MuiAutocomplete.tsx
@@ -6,6 +6,11 @@ export const MuiAutocomplete = () => {
     const skills:string[] = ['HTML','Javascript','Typescript','React Formik','React Router']
 
     const [value, setValue] = useState<string | null>(null)
+
+    console.log(value)
+    const handlechange = (event:any,newvalue:string | null) =>{
+      
+    }
   return (
     <Stack spacing={2} width="250px">
         <Autocomplete options={skills} value={value} onChange={handlechange} renderInput={(params)=><TextField {...params} label="Skills"/>}/>
