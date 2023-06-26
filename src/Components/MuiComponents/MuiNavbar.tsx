31f7b605520c905e4e6ced1dfe25c92d56bc78d1
rinel <rineliniguezsosa@gmail.com>
2023-06-26 01:55:25 -0600

    fix(MuiNavbar): corrigiendo el nombre del funcional component MuiNavbar por Muinavbar

--- src/Components/MuiComponents/MuiNavbar.tsx
+++ src/Components/MuiComponents/MuiNavbar.tsx
@@ -1,7 +1,7 @@
 import { AppBar,Toolbar,Stack,Typography,IconButton,Button } from '@mui/material'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-export const MuiNavbar = () => {
+export const Muinavbar = () => {
   return (
     <AppBar position="static">
         <Toolbar>
