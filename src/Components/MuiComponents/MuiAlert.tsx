6705478bc871ff4d4f1a68650bad53011663743b
rinel <rineliniguezsosa@gmail.com>
2023-07-03 16:34:34 -0600

    feat(MuiAlert): estableciendo el componente Alert
    -propiedad variant="outlined" para definir el alert redondeado
    -propiedad severity="success"

--- src/Components/MuiComponents/MuiAlert.tsx
+++ src/Components/MuiComponents/MuiAlert.tsx
@@ -4,6 +4,8 @@ export const MuiAlert = () => {
   return (
     <Stack spacing={2}>
         <Alert variant="standard" severity='success'>Alert</Alert>
+
+        <Alert variant="outlined" severity='success'>Alert</Alert>
     </Stack>
   )
 }
