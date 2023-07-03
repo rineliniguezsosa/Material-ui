c29125f0baf1ca6caabbeaae5d20f1dbd21f2a97
rinel <rineliniguezsosa@gmail.com>
2023-07-03 16:32:11 -0600

    feat(MuiAlert): estableciendo el componente Alert
    -estableciendo la propiedad variant="standard"
    -estableciendo la propiedad severity="success"

--- src/Components/MuiComponents/MuiAlert.tsx
+++ src/Components/MuiComponents/MuiAlert.tsx
@@ -3,7 +3,7 @@ import { Stack,Alert,AlertTitle } from '@mui/material'
 export const MuiAlert = () => {
   return (
     <Stack spacing={2}>
-
+        <Alert variant="standard" severity='success'>Alert</Alert>
     </Stack>
   )
 }
