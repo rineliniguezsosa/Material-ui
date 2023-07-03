44ad87682afafe42e8a682d2e6de284adc549280
rinel <rineliniguezsosa@gmail.com>
2023-07-03 17:18:54 -0600

    feat(MuiSnackbar): estableciendo el componente Snackbar
    -propiedad message="Rinel say Hellow" sera el mensaje por mostrara al usuario final
    -propiedad open={open} sera el valor boolean true o false para desplegar el componente Snackabar
    -propiedad autoHideDuration={2000} sera el tiempo en milisegundos de duración del message
    -propiedad onClose={handleclose} referencia de la funcion handleclose para cerrar el componente Snackbar

--- src/Components/MuiComponents/MuiSnackbar.tsx
+++ src/Components/MuiComponents/MuiSnackbar.tsx
@@ -5,6 +5,12 @@ export const MuiSnackbar = () => {
   return (
     <>
         <Button>Submit</Button>
+        <Snackbar
+            message="Rinel say Hellow"
+            open={open}
+            autoHideDuration={2000}
+            onClose={handleclose}
+        />
     </>
   )
 }
