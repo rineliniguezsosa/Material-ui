6eddf2f0c82201df846185567503a2066f1d820b
rinel <rineliniguezsosa@gmail.com>
2023-07-03 18:38:39 -0600

    feat(MuiSnackbar): comentando el Snackbar de la linea 22 a la 27

--- src/Components/MuiComponents/MuiSnackbar.tsx
+++ src/Components/MuiComponents/MuiSnackbar.tsx
@@ -19,12 +19,12 @@ export const MuiSnackbar = () => {
   return (
     <>
         <Button onClick={()=> setOpen(true)}>Submit</Button>
-        <Snackbar
+        {/* <Snackbar
             message="Rinel say Hellow"
             open={open}
             autoHideDuration={2000}
             onClose={handleclose}
-        />
+        /> */}
 
         <Snackbar open={open} autoHideDuration={4000} onClose={handleclose}>
             <SnackbarAlert onClose={handleclose} variant='filled' severity="success">
