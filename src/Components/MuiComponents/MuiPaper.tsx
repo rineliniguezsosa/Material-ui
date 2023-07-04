63107c01f70ca290cf230524ca92e7366b832d38
rinel <rineliniguezsosa@gmail.com>
2023-07-04 16:18:09 -0600

    feat(MuiPaper): estableciendo el componente Paper de material ui
    -propiedad sx para establecer atributos css, width:'50%', padding:'10px', height:100
    -propiedad elevation para definir la profundidad de la sombra del componente Paper

--- src/Components/MuiComponents/MuiPaper.tsx
+++ src/Components/MuiComponents/MuiPaper.tsx
@@ -3,7 +3,9 @@ import { Grid,Paper } from "@mui/material"
 export const MuiPaper = () => {
   return (
     <Grid container>
-        
+        <Paper sx={{width:'50%',padding:'10px',height:100}} elevation={3}>
+            rinel
+        </Paper>
     </Grid>
   )
 }
