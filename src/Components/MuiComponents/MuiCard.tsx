c34b1141a7c647b5d6d4d306d740125d775f01b8
rinel <rineliniguezsosa@gmail.com>
2023-07-04 16:58:54 -0600

    feat(MuiCard): estableciendo el componente Card

--- src/Components/MuiComponents/MuiCard.tsx
+++ src/Components/MuiComponents/MuiCard.tsx
@@ -3,7 +3,9 @@ import { Box,Typography,Card,CardContent } from "@mui/material"
 export const MuiCard = () => {
   return (
     <Box width="300px">
-
+        <Card>
+          
+        </Card>
     </Box>
   )
 }
