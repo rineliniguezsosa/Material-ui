54462c6d21fbde6859a3c85169c6550db85d7036
rinel <rineliniguezsosa@gmail.com>
2023-07-04 21:13:47 -0600

    feat(MuiCard): estableciendo el componente Typography con el lorem ipsum como titulo
    -propiedad variant="body2" para establecer el titulo de la letra
    -color="InfoText" para establecer el color del texto

--- src/Components/MuiComponents/MuiCard.tsx
+++ src/Components/MuiComponents/MuiCard.tsx
@@ -6,6 +6,10 @@ export const MuiCard = () => {
         <Card>
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">React</Typography>
+
+              <Typography variant="body2" color="InfoText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie egestas dui, in commodo nisi aliquet eget. Nunc sit amet lorem eget 
+              </Typography>
             </CardContent>
         </Card>
     </Box>
