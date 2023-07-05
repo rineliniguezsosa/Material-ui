5b2bfe799c64d1de84667965325b1950f32e3bc6
rinel <rineliniguezsosa@gmail.com>
2023-07-04 23:57:18 -0600

    feat(MuiLink): estableciendo el componente Stack
    -prop spacing={2} espacio entre los elementos hijos
    -prop direction="row" define los elementos hijoshorizontalmente
    -prop m={4} define el margen

--- src/Components/MuiComponents/MuiLink.tsx
+++ src/Components/MuiComponents/MuiLink.tsx
@@ -2,6 +2,8 @@ import { Stack,Link,Typography } from '@mui/material'
 
 export const MuiLink = () => {
   return (
-    <div>MuiLink</div>
+    <Stack spacing={2} direction="row" m={4}>
+
+    </Stack>
   )
 }
