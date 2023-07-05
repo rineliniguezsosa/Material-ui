44efc0d923962c86e51530d05a349e3f8d1411d2
rinel <rineliniguezsosa@gmail.com>
2023-07-04 23:22:57 -0600

    feat(MuiCard): creando una copia del componente Card en la forma basica

--- src/Components/MuiComponents/MuiCard.tsx
+++ src/Components/MuiComponents/MuiCard.tsx
@@ -22,6 +22,19 @@ export const MuiCard = () => {
               <Button size="small">Share</Button>
             </CardActions>
         </Card>
+
+        <Card>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">React</Typography>
+
+              <Typography variant="body2" color="InfoText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie egestas dui, in commodo nisi aliquet eget. Nunc sit amet lorem eget 
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+            </CardActions>
+        </Card>
     </Box>
   )
 }
