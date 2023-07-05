5b932e9ad117f20ecf754a2042ff51d0baf49eb9
rinel <rineliniguezsosa@gmail.com>
2023-07-05 00:04:55 -0600

    feat(MuiLink): estableciendo el componente Link con el titulo Home
    -prop href="#"
    -prop variant="body2" define el stilo de la fuente
    -prop underline='none' esta propiedad quita el subrayado si esta en none

--- src/Components/MuiComponents/MuiLink.tsx
+++ src/Components/MuiComponents/MuiLink.tsx
@@ -3,7 +3,7 @@ import { Stack,Link,Typography } from '@mui/material'
 export const MuiLink = () => {
   return (
     <Stack spacing={2} direction="row" m={4}>
-
+        <Link href="#" variant="body2" underline='none'>Home</Link>
     </Stack>
   )
 }
